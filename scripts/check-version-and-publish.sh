26e8924d988e112bc9467cf362894306b2d4af1c
Gokhan Egri <egrigokhan@gmail.com>
2023-08-03 07:32:49 -0400

    Update check-version-and-publish.sh

--- scripts/check-version-and-publish.sh
+++ scripts/check-version-and-publish.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Get the list of changed package.json files
-CHANGED_FILES=$(git diff --name-only ${{ github.event.before }} ${{ github.sha }} | grep 'packages/pieces/.*/package.json')
+# CHANGED_FILES=$(git diff --name-only ${{ github.event.before }} ${{ github.sha }} | grep 'packages/pieces/.*/package.json')
+
+npm run publish-piece airtable
 
 # Loop through the changed files
-for file in $CHANGED_FILES
-do
+# for file in $CHANGED_FILES
+# do
   # Extract the piece name
-  piece=$(echo $file | sed -n 's|packages/pieces/\(.*\)/package.json|\1|p')
+  # piece=$(echo $file | sed -n 's|packages/pieces/\(.*\)/package.json|\1|p')
 
-  echo $piece
-
+  # echo $piece
+  '''
   npm run publish-piece airtable
   
   # Check if the version number has changed
@@ -20,3 +22,4 @@ do
     npm run publish-piece $piece
   fi
 done
+'''
