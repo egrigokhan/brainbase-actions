6327be8a205bf886cefc61ff91276eba06b37896
Gokhan Egri <egrigokhan@gmail.com>
2023-08-03 06:51:09 -0400

    Create check-version-and-publish.sh

---
+++ scripts/check-version-and-publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the list of changed package.json files
+CHANGED_FILES=$(git diff --name-only ${{ github.event.before }} ${{ github.sha }} | grep 'packages/pieces/.*/package.json')
+
+# Loop through the changed files
+for file in $CHANGED_FILES
+do
+  # Extract the piece name
+  piece=$(echo $file | sed -n 's|packages/pieces/\(.*\)/package.json|\1|p')
+
+  # Check if the version number has changed
+  if git diff ${{ github.event.before }} ${{ github.sha }} -- $file | grep '"version":'
+  then
+    # Run the publishing command for the piece
+    npm run publish-piece $piece
+  fi
+done
