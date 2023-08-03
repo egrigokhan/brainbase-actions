b94e6f3cee9129660e2aa56e5a024afd5bcf4986
Gokhan Egri <egrigokhan@gmail.com>
2023-08-03 07:05:07 -0400

    Update check-version-and-publish.sh

--- scripts/check-version-and-publish.sh
+++ scripts/check-version-and-publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the list of changed package.json files
-CHANGED_FILES=$(git diff --name-only ${{ github.event.before }} ${{ github.sha }} | grep 'packages/pieces/.*/package.json')
+CHANGED_FILES=$(git diff --name-only $GITHUB_BEFORE $GITHUB_SHA | grep 'packages/pieces/.*/package.json')
 
 # Loop through the changed files
 for file in $CHANGED_FILES
@@ -9,13 +9,9 @@ do
   # Extract the piece name
   piece=$(echo $file | sed -n 's|packages/pieces/\(.*\)/package.json|\1|p')
 
-  echo "Checking file: $file"
-  echo "Piece: $piece"
-
   # Check if the version number has changed
-  if git diff ${{ github.event.before }} ${{ github.sha }} -- $file | grep '"version":'
+  if git diff $GITHUB_BEFORE $GITHUB_SHA -- $file | grep '"version":'
   then
-    echo "Running..."
     # Run the publishing command for the piece
     npm run publish-piece $piece
   fi
